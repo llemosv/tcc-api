1ba31dbc96d70b3703ec5ffb1fab11f134e43881
Leonardo Lemos <leonardo.lemos2002@hotmail.com>
2024-11-01 10:27:12 -0300

    alterando config cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.APP_URL,
+    origin: [process.env.APP_URL],
   });
   app.setGlobalPrefix('api');
 
