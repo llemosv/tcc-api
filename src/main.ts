3d7bc34f1554db3b1d3701a00b554dc56cf5c942
Leonardo Lemos <leonardo.lemos2002@hotmail.com>
2024-11-01 10:21:03 -0300

    adicionando url app nos cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.APP_URL,
+  });
   app.setGlobalPrefix('api');
 
   await app.listen(3599, () => {
