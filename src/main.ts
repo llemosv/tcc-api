e563ae4b67c14741f429f727a7a83212e4617d92
Leonardo Lemos <leonardo.lemos2002@hotmail.com>
2024-11-01 09:57:36 -0300

    alterando start do projeto

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(3000);
 
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  await app.listen(3599, () => {
+    console.log(
+      '\x1b[33m%s\x1b[0m',
+      `=> 🚀 ApiGerenciaSolicitacaoTcc RUNNING ON PORT: 3599`,
+    );
+  });
 }
 bootstrap();
